2fa8c5cc77893945b91ec736a0a16191f25f7380
nag432 <nag432@users.noreply.github.com>
2017-04-19 15:23:25 +0530

    Update consul_update.sh

--- consul_update.sh
+++ consul_update.sh
@@ -1,10 +1,14 @@
 PrjName=$1
 EnvName=$2
+TagName=$3
 
-echo $PrjName $EnvName
-
-sed  s/PrjName/$1/g  git2consul.json >  $PrjName_git2consul.json
-sed s/EnvName/$2/g  $PrjName_git2consul.json > $EnvName.json
-#cat $PrjName_git2consul_$EnvName.json
-sudo git2consul --config-file $EnvName.json -n
-#sudo git2consul --config-file /tmp/$PrjName_git2consul.json -n
+if [ $# -eq 3 ]; then
+    echo "Your command line contains $# arguments"
+    echo $PrjName $EnvName
+    sed  s/PrjName/$1/g  git2consul.json >  $PrjName_git2consul.json
+    sed s/EnvName/$2/g  $PrjName_git2consul.json > $EnvName.json
+    sed s/TagName/$3/g  $EnvName.json > $TagName.json
+    sudo git2consul --config-file $TagName.json -n	
+else
+    echo "Your command line contains no arguments"
+fi
